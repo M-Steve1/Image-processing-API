58f5b54509a285af95f2aa4a0d970375de951054
Modebe Stephen <modebestephen2@gmail.com>
2022-12-15 06:35:53 +0100

    Use sharp to implement resizing

--- src/controllers/resizeImgController.ts
+++ src/controllers/resizeImgController.ts
@@ -1,8 +1,18 @@
 import express from "express";
+import path from "path";
+import sharp from "sharp";
+
+const input = path.resolve("images/full/fjord.jpg");
+console.log(input);
+const output = path.resolve("images/resized/fjord.jpg");
+console.log(output);
 
 export const resizeImage = (
   req: express.Request,
   res: express.Response
 ): void => {
-  res.send("Resize end point is working").status(200);
-};
+  sharp(input)
+  .resize({width: 200, height:300})
+  .toFile(output);
+  res.send("ok").status(200);
+}
