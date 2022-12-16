7193b56646746e9ba468a4c0bdae3b653ddcc2d8
Modebe Stephen <modebestephen2@gmail.com>
2022-12-16 21:20:07 +0100

    Build js files from ts

--- dist/utilities/fileDetails.js
+++ dist/utilities/fileDetails.js
@@ -2,27 +2,27 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getFileExt = exports.getFileName = void 0;
 const getFileName = (input) => {
-    const splitFilePath = input.split(".");
+    const splitFilePath = input.split('.');
     // The path without the extension name
     const filePath = splitFilePath[0];
     // Getting the file name alone.
-    let fileName = "";
+    let fileName = '';
     for (let i = filePath.length - 1; i > 0; i--) {
-        if (filePath[i] === "/" || filePath[i] === "\\") {
+        if (filePath[i] === '/' || filePath[i] === '\\') {
             break;
         }
         else {
             fileName += filePath[i];
         }
     }
-    fileName = fileName.split("").reverse().join("");
+    fileName = fileName.split('').reverse().join('');
     return fileName;
 };
 exports.getFileName = getFileName;
 const getFileExt = (input) => {
-    const splitFilePath = input.split(".");
+    const splitFilePath = input.split('.');
     let fileExt = splitFilePath[splitFilePath.length - 1];
-    fileExt = "." + fileExt;
+    fileExt = '.' + fileExt;
     return fileExt;
 };
 exports.getFileExt = getFileExt;
